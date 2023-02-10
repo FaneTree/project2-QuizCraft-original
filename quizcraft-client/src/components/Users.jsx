ef80e5ecc8fae265e6d3110d3a50048365ac279f
Haoran-ryan <90777296+Haoran-ryan@users.noreply.github.com>
2023-02-10 12:20:49 +1100

    Users component created

---
+++ quizcraft-client/src/components/Users.jsx
@@ -0,0 +1,11 @@
+import React,{ Component } from "react";
+
+const User = ()=>{
+    return(
+        <div>
+            User authentication is coming        
+        </div>
+    )
+}
+
+export default User;
