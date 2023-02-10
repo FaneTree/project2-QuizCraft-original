d149c31642fe93c8b286f9d8fc2aa5272d2adf53
Haoran-ryan <90777296+Haoran-ryan@users.noreply.github.com>
2023-02-10 16:34:22 +1100

    sign in module imported to Login component

--- quizcraft-client/src/components/Login.jsx
+++ quizcraft-client/src/components/Login.jsx
@@ -1,12 +1,27 @@
 import React, {Component, useState} from 'react';
 import { NavLink, useNavigate } from 'react-router-dom';
-import {  createUserWithEmailAndPassword  } from 'firebase/auth';
+import {  signInWithEmailAndPassword  } from 'firebase/auth';
 import { auth } from './firebase.js';
 
 const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const logInWithEmailAndPassword = async (email, password) => {
+        try {
+          await signInWithEmailAndPassword(auth, email, password);
+        } catch (err) {
+          console.error(err);
+          alert(err.message);
+        }
+      };
+
     return (
         <div>
-            login coming 
+            <form>
+                <input type="text" placeholder='Enter your email' />
+                <input type="password" placeholder='Enter your password' />
+            </form>
         </div>
     )
 }
