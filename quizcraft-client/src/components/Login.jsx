c75ffc092569b54f66367074410c9c7567b24262
Haoran-ryan <90777296+Haoran-ryan@users.noreply.github.com>
2023-02-10 16:56:50 +1100

    login working on Firebase but it's not redirected to a correct page

--- quizcraft-client/src/components/Login.jsx
+++ quizcraft-client/src/components/Login.jsx
@@ -7,20 +7,39 @@ const Login = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
-    const logInWithEmailAndPassword = async (email, password) => {
-        try {
-          await signInWithEmailAndPassword(auth, email, password);
-        } catch (err) {
-          console.error(err);
-          alert(err.message);
-        }
-      };
+
+    // const logInWithEmailAndPassword = async (email, password) => {
+    //     try {
+    //       await signInWithEmailAndPassword(auth, email, password);
+    //     } catch (err) {
+    //       console.error(err);
+    //       alert(err.message);
+    //     }
+    //   };
+
+    const _handleSubmit = (e) => {
+        e.preventDefault();
+        // setEmail('');
+        // setPassword('');
+
+        signInWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                // Signed in
+                const user = userCredential.user;
+                // ...
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+            });
+    }
 
     return (
         <div>
-            <form>
-                <input type="text" placeholder='Enter your email' />
-                <input type="password" placeholder='Enter your password' />
+            <form onSubmit={ _handleSubmit }>
+                <input type="text" placeholder='Enter your email' name="email" value={ email } onInput={ (e) => setEmail( e.target.value ) }/>
+                <input type="password" placeholder='Enter your password' value={ password } onInput={ (e) =>  setPassword( e.target.value ) }/>
+                <input type="submit" value="Log In" />
             </form>
         </div>
     )
