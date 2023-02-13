458c2e2d08b5ab8fac35c1baa385bf8145c626f2
JP <jpcodingjourney@192-168-1-104.tpgi.com.au>
2023-02-13 18:48:37 +1100

    MultiMode v1

--- quizcraft-client/src/components/App.js
+++ quizcraft-client/src/components/App.js
@@ -1,41 +1,32 @@
 // import Users from './User/Users';
-import React from 'react';
+import React from "react";
 import { Routes, Route } from "react-router-dom";
 import "../App.scss";
 
-import NavBar from "./NavBar"
-import Signup from './Routes/Signup';
-import Signin from './Routes/Signin';
-import Home from './Routes/Home';
-import Quizmaster from './Quiz/Quizmaster';
-import Quiz from './Quiz/Quiz';
+import NavBar from "./NavBar";
+import Signup from "./Routes/Signup";
+import Signin from "./Routes/Signin";
+import Home from "./Routes/Home";
+import Quizmaster from "./Quiz/Quizmaster";
+import Quiz from "./Quiz/Quiz";
+import Lobby from "./Routes/Lobby";
 
 function App() {
-
-    return (
-        <div className="App">
-            <NavBar />
-            <div className = "Content">
-                <Routes>
-                    <Route exact path="/" element={
-                            <Home />
-                    } />
-                    <Route exact path="/signup" element={
-                            <Signup />
-                    } />
-                    <Route exact path="/signin" element={
-                            <Signin />
-                    } />
-                    <Route exact path="/create" element={
-                            <Quizmaster />
-                    } />
-                    < Route exact path="/quiz" element={
-                        <Quiz />
-                    } />
-                </Routes>
-            </div>
-        </div>
-    );
+  return (
+    <div className="App">
+      <NavBar />
+      <div className="Content">
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/signup" element={<Signup />} />
+          <Route exact path="/signin" element={<Signin />} />
+          <Route exact path="/create" element={<Quizmaster />} />
+          <Route exact path="/lobby" element={<Lobby />} />
+          <Route exact path="/quiz" element={<Quiz />} />
+        </Routes>
+      </div>
+    </div>
+  );
 }
 
 export default App;
