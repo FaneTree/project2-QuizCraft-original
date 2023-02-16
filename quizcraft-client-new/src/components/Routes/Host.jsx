4db2b1b9bbbff6a895e161c44c072b37dd1f3993
Haoran-ryan <90777296+Haoran-ryan@users.noreply.github.com>
2023-02-16 12:35:31 +1100

    host can control the question using next button'

--- quizcraft-client-new/src/components/Routes/Host.jsx
+++ quizcraft-client-new/src/components/Routes/Host.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom"
-import { doc, onSnapshot } from 'firebase/firestore';
+import { doc, onSnapshot, updateDoc } from 'firebase/firestore';
 import { db } from "../firebase";
 
 import Quiz from '../component/Quiz';
@@ -11,17 +11,56 @@ import WaitingRoom from "../component/WaitingRoom";
 export default function Host () {
     const [showQuiz, setShowQuiz] = useState(false);
     const [gameStatus, setGameStatus] = useState(false);
+    const [currentQuestion, setCurrentQuestion] = useState(0);
+    const [roomData, setRoomData] = useState([]);
 
     let { gameId, playerId } = useParams();
     const gameID = gameId;
+    const gamesDocRef = doc(db, "games", gameID.toString());
 
     const unsub = onSnapshot(doc(db, "games", gameID), (doc) => {
         console.log("Current data: ", doc.data());
     });
+    unsub();
 
     const _showQuiz = () => {
         setShowQuiz(!showQuiz);
         console.log(showQuiz);
+    }
+
+    // onSnapshot to fetch roomData
+    useEffect(() => {
+        const unsubRoomData = onSnapshot(gamesDocRef,
+            snapShot => {
+                setRoomData(snapShot.data().room);
+            });
+        return () => unsubRoomData();
+    }, []);
+
+    // use OnSnapshot to fetch currentQuestion from Database;
+    useEffect(
+        ()=>{
+            const unsubCurrentQuestion = onSnapshot(gamesDocRef,
+                snapshot =>
+                    setCurrentQuestion(snapshot.data().room.currentQuestion)
+            );
+            return () => unsubCurrentQuestion();
+        },[])
+
+    useEffect(()=>{
+        console.log("current Question Number: ", currentQuestion)
+    },[currentQuestion])
+
+    const _handleClick = ()=>{
+        setCurrentQuestion( currentQuestion + 1 );
+
+        // update the currentQuestion number in the firestore
+        updateDoc(doc(db, "games", gameID.toString()),{
+            room:{
+                ...roomData,
+                currentQuestion: currentQuestion + 1 
+            }
+        })
     }
     
     return (
@@ -31,7 +70,7 @@ export default function Host () {
                 <div>
                     <Quiz a = { gameID } /> 
                     {/* change current question + 1*/}
-                    <button>Next</button>
+                    <button onClick={ _handleClick } >Next</button>
                     <button onClick = { _showQuiz }>Lobby</button>
                 </div>
             }
